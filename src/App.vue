fc13847be6d1d62d28227bc4b59f205498038dcc
morgan <mog13@live.com>
2019-03-12 16:44:06 +0000

    added initial timeline

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+
+        <time-line :events="timelineData.events" :current="currentTime" v-on:timeChanged="updateTime" v-on:start="startTimeLine" v-on:stop="stopTimeline"></time-line>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    import TimeLine from "./components/TimeLine";
+    import timelineData from "./assets/events.json";
+
+    export default {
+        name: 'app',
+        components: {
+            TimeLine
+        }, data: () => {
+            return {
+                timelineData,
+                currentTime: {year: 1992, month: 1},
+                tick:null,
+                tickTime:1000
+            }
+        },
+        methods: {
+            updateTime: function (newTime) {
+                this.currentTime = newTime;
+            },
+            startTimeLine: function () {
+                this.tick = setInterval(this.tickTimeline,this.tickTime)
+            },
+            stopTimeline: function () {
+                if(this.tick) clearInterval(this.tick)
+            },
+            tickTimeline:function () {
+                this.currentTime.month++;
+                if( this.currentTime.month>12) {
+                    this.currentTime.month =1;
+                    this.currentTime.year++;
+                }
+            }
+        }
+
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/TimeLine.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <p> curent date: {{this.current}}</p>
+        <button v-on:click="startTimeline()" :disabled="started">start</button>
+        <button v-on:click="stopTimeline()" :disabled="!started">stop</button>
+        <div class="blocks">
+            <div class="block" @click="eventClicked(event)" v-for="event in events" :key="event.time.year"> {{event.name}}</div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TimeLine",
+        props: ["events", "current"],
+        data:()=>{
+            return {
+                started:false
+            }
+        },
+        methods:{
+            eventClicked:function(e){
+                this.$emit('timeChanged',e.time)
+            },
+            startTimeline:function () {
+                this.started = true;
+                this.$emit('start')
+            },
+            stopTimeline:function () {
+                this.started = false;
+                this.$emit('stop')
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .blocks{
+        display: flex;
+
+        .block{
+            background-color:blue;
+            color:white;
+            margin:5px;
+        }
+    }
+</style>
