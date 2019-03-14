d5f865e42526ef2ad77d4a4a3b73d00364a0e6cb
morgan <mog13@live.com>
2019-03-14 13:38:19 +0000

    links module css

--- src/components/modules/links.vue
+++ src/components/modules/links.vue
@@ -2,9 +2,10 @@
     <div class="link-container">
 
 
-    <div v-for="link in links" >
-        <h1>{{link.name}}</h1>
-        <a target="_blank" :href="'http://'+link.content">{{link.content}}</a>
+    <div class="link" v-for="link in links" >
+        <a class="title" v-if="link.href" target="_blank" :href="link.href">{{link.name}}</a>
+        <h1 class="title" v-else>{{link.name}}</h1>
+        <h2 class="content" v-if="link.content">{{link.content}}</h2>
     </div>
     </div>
 </template>
@@ -16,6 +17,22 @@
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    @import "../../scss/colours";
 
+    .link-container {
+     padding-top:20px;
+        .link {
+            padding-bottom:20px;
+            .title {
+                font-size: 1.5rem;
+                font-weight: bold;
+                color: $module-accent;
+            }
+            .content {
+                font-size: 1rem;
+                color: $module-light;
+            }
+        }
+    }
 </style>
