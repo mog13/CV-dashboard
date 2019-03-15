b5c6e9d0833406090c0fdb6e246d18af3f26ca2e
morgan <mog13@live.com>
2019-03-15 15:34:02 +0000

    added events for graph

--- src/components/modules/graph.vue
+++ src/components/modules/graph.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="graphContainer">
         <div class="graphSize">
-            <line-chart class="graph" id="line" :data="proccesedData"  :xkey="'date'" :ykeys="['test','test2']" resize="true"></line-chart>
+            <line-chart class="graph" id="line" :data="proccesedData" :hoverCallback="genLegend" :xkey="'year'" :labels="skills" :ykeys="skills" resize="true"></line-chart>
         </div>
         <no-data v-if="proccesedData.length ===0"></no-data>
     </div>
@@ -19,15 +19,53 @@
         },
         props: ["graphData"],
         data: () => {
-            return {}
+            return {
+                skills:['js','csharp','ts','java','c'],
+                labels:["JS","C#","TS","Java","C++"]
+            }
         },
         computed: {
             proccesedData: function () {
-                return this.graphData.filter(d=>{return d.data.js || d.data.csharp}).map(data => {
-                    return {date: `${data.time.year}.${data.time.month}`,
-                        csharp: data.data.csharp,
-                        js:data.data.js}
+                let vm = this;
+                return this.graphData.filter(d=>{
+                    let graphable = false;
+                    vm.skills.forEach(skill=>{
+                        if(d.data[skill]) graphable = true;
+                    })
+                    return graphable;
+                }).map(data => {
+                    let dataPoint ={year: data.time.year,
+                    month:data.time.month};
+                    this.skills.forEach(skill=>{
+                        if(data.data[skill]) dataPoint[skill] = data.data[skill];
+                    });
+                        return dataPoint
+
                 })
+            }
+        },
+        methods:{
+            genLegend:function(index, options, content, row){
+
+                let newContent = `<div class='morris-hover-row-label'>${row.month}/${row.year}</div>`;
+                this.skills.forEach((skill,i)=>{
+                    newContent+=this.createEntry(row,skill,this.labels[i],options.lineColors[i]);
+                });
+                return newContent
+            },
+            createEntry:function (row, param, label,col) {
+                let amount = row[param];
+                if(!amount) return "";
+                let level = "beginner";
+                if(amount > 3) level = "amateur";
+                if(amount > 6) level = "novice";
+                if(amount > 8) level = "intermediate";
+                if(amount > 10) level = "strong";
+                if(amount > 13) level = "advanced";
+                if(amount >= 15) level = "expert";
+
+
+               return `<div class='morris-hover-point' style="color:${col}; font-weight: bold">${label}: ${level}</div>`
             }
         }
     }
